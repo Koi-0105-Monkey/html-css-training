2b25a62d10d05879eaca9433af69e734667f6754
Koi-12-love <khoihuynh152004@gmail.com>
2025-07-15 16:12:33 +0700

    Done service UI

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,3 +15,9 @@ fetch(new URL('../components/partner.html', import.meta.url))
   .then(data => {
     document.getElementById('partner-container').innerHTML = data;
   })
+
+  fetch(new URL('../components/service.html', import.meta.url))
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById('service-container').innerHTML = data;
+  })
