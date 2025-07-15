f3e931eda5776cef9890046a35669bfe01659ed1
Koi-12-love <khoihuynh152004@gmail.com>
2025-07-15 15:05:46 +0700

    Done partner UI

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,3 +9,9 @@ fetch(new URL('../components/hero.html', import.meta.url))
   .then(data => {
     document.getElementById('hero-container').innerHTML = data;
   })
+
+fetch(new URL('../components/partner.html', import.meta.url))
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById('partner-container').innerHTML = data;
+  })
